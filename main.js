508c060487dc8d24b559224a002bf7cc49c4ef24
Eugenio González <egzza@Eugenios-MacBook-Pro.local>
2020-09-11 15:48:20 -0500

    Added colors

--- main.js
+++ main.js
@@ -16,8 +16,7 @@ document.getElementById("fButton").addEventListener('click',(event)=>{
         if(!(Number.isNaN(price)||price<0||price=="")){
             console.log("Valid price")
             f_price.className="valid"
-            let el = get_element_li(item,price)
-            addToList(el)
+            addToList(item,price)
             let curr=Number(total.innerHTML)
             total.innerHTML = price+curr
             f_item.value=""
@@ -46,10 +45,12 @@ document.body.addEventListener('click',(event)=>{
 })
 
 // Append new item to list
-function addToList(list_item){
+function addToList(item,price){
+
     let node_item=document.createElement("li")
     node_item.className = "list-item"
-    //node_item.attributes('data-price', )
+    node_item.setAttribute("data-price", price)
+    let list_item = get_element_li(item,price)
     node_item.innerHTML += list_item
     let lst = document.getElementById("list")
     lst.appendChild(node_item)
@@ -57,7 +58,7 @@ function addToList(list_item){
 
 // List item template
 function get_element_li (item, price) {
-    return `Item: ${item}  |   Price: ${price}   <button class="remove-item">❌</button>`
+    return `Item: ${item} |   Price: ${price}   <button class="remove-item">❌</button>`
 }
   
 // Remove child node
@@ -70,16 +71,11 @@ let remove_item  = (node_to_remove) => {
 
 // Subtract from total
 function subtractFromTotal(el){
-    let line = el.innerHTML
+    //let line = el.innerHTML
     let total=document.getElementById("cost")
     let curr=Number(total.innerHTML)
-    console.log("curr ",curr)
-    console.log(line)
-    line=line.substr(line.search("Price:")+7)
-    console.log(line)
-    line=line.substr(0,line.search("<"))
-    console.log(line)
-    price=Number(line)
+
+    let price = el.getAttribute("data-price")
 
     total.innerHTML = curr-price
 }
